0ae4f5b609e37036a977a22a5d83845e4b3f1fdb
Joaquín Rodrigo Araya Monsalve <joa.arayam@duocuc.cl>
2025-11-30 04:11:18 -0300

    formatocasi

--- app/src/main/java/com/example/diegoherrera22appmoviles007d_ev2_dherrera_jaraya/views/RegisterScreen.kt
+++ app/src/main/java/com/example/diegoherrera22appmoviles007d_ev2_dherrera_jaraya/views/RegisterScreen.kt
@@ -13,14 +13,14 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.ExposedDropdownMenu
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf

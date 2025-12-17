d4663f8f4ce0d6a2bd2bf3c44644bb31c86e3ee2
Joaquín Rodrigo Araya Monsalve <joa.arayam@duocuc.cl>
2025-12-17 07:04:57 -0300

    admin listo

--- app/src/main/java/com/example/diegoherrera22appmoviles007d_ev2_dherrera_jaraya/views/backoffice/BackOfficeListScreen.kt
+++ app/src/main/java/com/example/diegoherrera22appmoviles007d_ev2_dherrera_jaraya/views/backoffice/BackOfficeListScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
@@ -278,6 +277,7 @@ fun BackOfficeListScreen(
                                         enabled = false,
                                         readOnly = true,
                                         label = { Text("Nutriente") },
+                                        modifier = Modifier
                                             .weight(1f)
                                             .padding(end = 4.dp),
                                         colors = pastelOutlinedTextFieldColors()
